8fc462e907d6e059de8b8f5c22d751b94390b32f
foxten <tenaysiafox@gmail.com>
2020-08-30 08:27:33 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,26 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = (props) =>{
+    console.log(props)
+    return (
+        <div className='review-list'>
+            <form onSubmit={props.handleSubmit}>
+                <input type='text' name='searchTerm' placeholder='Enter film title here'/>
+                <input type='submit' value="Submit"/>
+            </form>
+                {props.reviews.map((film) =>{
+                    return (
+                        <div className='review'>
+                            <h3>Film: {film.display_title}</h3>
+                            <h5>Headline: {film.headline}</h5>
+                            <h5>Critic: {film.byline}</h5>
+                            <h5><a href={film.link.url}>Full Review Here</a></h5>
+                            <h5>Rating: {film.mpaa_rating}</h5>
+                        </div>
+                    )
+                })}
+        </div>
+    )
+}
+
+export default MovieReviews
